c2429a603902e644eaad7af7e3a81750b0c6c657
Boldizsar Nemeth <boldizsarn@sqreamtech.com>
2022-08-05 09:55:43 +0200

    Refactor lsp server

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,10 @@
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 import Language.LSP.Server as LSP
 import Language.LSP.Types as LSP
@@ -10,6 +14,7 @@ import Language.LSP.Types.Lens as LSP (uri, params, settings)
 import Control.Monad.IO.Class
 import Control.Monad
 import Control.Exception
+import Control.Monad.Reader
 import qualified Data.Text as T
 import Data.Maybe (fromMaybe, isNothing)
 import Data.String (IsString(..))
@@ -25,118 +30,130 @@ import Data.Vector (toList)
 
 handlers :: Handlers (LspM Config)
 handlers = mconcat
-  [ notificationHandler SInitialized $ \_ -> tryToConnectToDB
-  , notificationHandler SWorkspaceDidChangeConfiguration $ \msg -> do
-      cfg <- LSP.getConfig
-      when (isNothing (connection cfg))
+  [ notificationHandler SInitialized $ \_ -> runInContext "Initialization" tryToConnectToDB
+  , notificationHandler SWorkspaceDidChangeConfiguration $ \msg -> runInContext "ChangeConfiguration" $ do
+      cfg <- liftLSP LSP.getConfig
+      when (isNothing (cfConnection cfg))
         tryToConnectToDB
       
-    -- config <- LSP.getConfig
-    -- case loadConfig config cfg of
-    --   Right updatedConfig -> LSP.setConfig updatedConfig
-    --   Left err -> sendNotification SWindowShowMessage (ShowMessageParams MtError err)
-
-  , requestHandler STextDocumentDefinition $ \req responder -> do
+  , requestHandler STextDocumentDefinition $ \req responder -> runInContext "Definition" $ do
       let RequestMessage _ _ _ (DefinitionParams (TextDocumentIdentifier uri) pos _ _) = req
           Position line column = pos
           lineNum = fromIntegral line + 1 :: Integer
           columnNum = fromIntegral column + 1 :: Integer
-      cfg <- LSP.getConfig
-      case (uriToFilePath uri, connection cfg) of
-        (Just file, Just conn) -> do
+      withConnection $ \conn ->
+        ensureFileLocation uri responder $ \file -> do
           names <- liftIO $ query
-              conn
-              "SELECT dm.filePath, d.startRow, d.startColumn \
-                  \FROM names AS n JOIN names AS d ON n.name = d.name JOIN modules nm ON n.module = nm.moduleId JOIN modules dm ON d.module = dm.moduleId \
-                  \WHERE nm.filePath = ? AND n.startRow <= ? AND n.endRow >= ? AND n.startColumn <= ? AND n.endColumn >= ? \
-                  \AND d.isDefined = TRUE"
-              ( file, lineNum, lineNum, columnNum, columnNum )
+            conn
+            "SELECT dm.filePath, d.startRow, d.startColumn \
+                \FROM names AS n JOIN names AS d ON n.name = d.name JOIN modules nm ON n.module = nm.moduleId JOIN modules dm ON d.module = dm.moduleId \
+                \WHERE nm.filePath = ? AND n.startRow <= ? AND n.endRow >= ? AND n.startColumn <= ? AND n.endColumn >= ? \
+                \AND d.isDefined = TRUE"
+            ( file, lineNum, lineNum, columnNum, columnNum )
           case names of
-              [] -> responder $ Left $ responseError "Definition not found"
-              (file, defLine :: Integer, defColumn :: Integer):_ -> 
-                let pos = Position (fromIntegral defLine - 1) (fromIntegral defColumn - 1)
-                in responder $ Right (InL (Location (filePathToUri file) (LSP.Range pos pos)))
-        (Nothing, _) -> responder $ Left $ responseError "Can't go to definition: Document is not a file"
-        (_, Nothing) -> responder $ Left $ responseError "Can't go to definition: No database connection"
+            [] -> liftLSP $ responder $ Left $ responseError "Definition not found"
+            (file, defLine :: Integer, defColumn :: Integer):_ -> 
+              let pos = Position (fromIntegral defLine - 1) (fromIntegral defColumn - 1)
+              in liftLSP $ responder $ Right (InL (Location (filePathToUri file) (LSP.Range pos pos)))
 
-  , requestHandler STextDocumentReferences $ \req responder -> do
+  , requestHandler STextDocumentReferences $ \req responder -> runInContext "References" $ do
       let RequestMessage _ _ _ (ReferenceParams (TextDocumentIdentifier uri) pos _ _ (ReferenceContext includeDefinition)) = req
           Position line column = pos
           lineNum = fromIntegral line + 1 :: Integer
           columnNum = fromIntegral column + 1 :: Integer
-      cfg <- LSP.getConfig
-      case (uriToFilePath uri, connection cfg) of
-        (Just file, Just conn) -> do
+      withConnection $ \conn ->
+        ensureFileLocation uri responder $ \file -> do
           names <- liftIO $ query
-              conn
-              (fromString $ "SELECT dm.filePath, d.startRow, d.startColumn \
-                  \FROM names AS n JOIN names AS d ON n.name = d.name JOIN modules nm ON n.module = nm.moduleId JOIN modules dm ON d.module = dm.moduleId \
-                  \WHERE nm.filePath = ? AND n.startRow <= ? AND n.endRow >= ? AND n.startColumn <= ? AND n.endColumn >= ?"
-                    ++ (if not includeDefinition then "\n AND d.isDefined = FALSE" else ""))
-              ( file, lineNum, lineNum, columnNum, columnNum )
+            conn
+            (fromString $ "SELECT dm.filePath, d.startRow, d.startColumn \
+                \FROM names AS n JOIN names AS d ON n.name = d.name JOIN modules nm ON n.module = nm.moduleId JOIN modules dm ON d.module = dm.moduleId \
+                \WHERE nm.filePath = ? AND n.startRow <= ? AND n.endRow >= ? AND n.startColumn <= ? AND n.endColumn >= ?"
+                  ++ (if not includeDefinition then "\n AND d.isDefined = FALSE" else ""))
+            ( file, lineNum, lineNum, columnNum, columnNum )
           let
             toPos line col = Position (fromIntegral line - 1) (fromIntegral col - 1) 
             lineToLoc :: (String, Integer, Integer) -> LSP.Location
             lineToLoc (file,line,col) = Location (filePathToUri file) (LSP.Range (toPos line col) (toPos line col))
-          responder $ Right $ LSP.List (map lineToLoc names)
-        (Nothing, _) -> responder $ Left $ responseError "Can't go to definition: Document is not a file"
-        (_, Nothing) -> responder $ Left $ responseError "Can't go to definition: No database connection"
+          liftLSP $ responder $ Right $ LSP.List (map lineToLoc names)
 
-
-  , requestHandler STextDocumentHover $ \req responder -> do
+  , requestHandler STextDocumentHover $ \req responder -> runInContext "Hover" $ do
       let RequestMessage _ _ _ (HoverParams (TextDocumentIdentifier uri) pos _workDone) = req
           Position line column = pos
           lineNum = fromIntegral line + 1 :: Integer
           columnNum = fromIntegral column + 1 :: Integer
-      cfg <- LSP.getConfig
-      case (uriToFilePath uri, connection cfg) of
-        (Just file, Just conn) -> do
+      withConnection $ \conn ->
+        ensureFileLocation uri responder $ \file -> do
           names <- liftIO $ query
-              conn
-              "SELECT n.isDefined, n.name, n.startRow, n.startColumn, n.endRow, n.endColumn \
-                  \FROM names n JOIN modules nm ON n.module = nm.moduleId \
-                  \WHERE nm.filePath = ? AND n.startRow <= ? AND n.endRow >= ? AND n.startColumn <= ? AND n.endColumn >= ?"
-              ( file, lineNum, lineNum, columnNum, columnNum )
+            conn
+            "SELECT n.isDefined, n.name, n.startRow, n.startColumn, n.endRow, n.endColumn \
+                \FROM names n JOIN modules nm ON n.module = nm.moduleId \
+                \WHERE nm.filePath = ? AND n.startRow <= ? AND n.endRow >= ? AND n.startColumn <= ? AND n.endColumn >= ?"
+            ( file, lineNum, lineNum, columnNum, columnNum )
           case names of
-              [] -> responder (Right Nothing)  
-              (isDefined :: Bool, name :: String, startLine :: Integer, startColumn :: Integer, endLine :: Integer, endColumn :: Integer):_ -> 
-                let ms = HoverContents $ markedUpContent "hstools" (T.pack $ name ++ if isDefined then " defined here" else "")
-                    range = LSP.Range (Position (fromIntegral startLine - 1) (fromIntegral startColumn - 1)) 
-                                      (Position (fromIntegral endLine - 1) (fromIntegral endColumn - 1))
-                    rsp = Hover ms (Just range)
-                in responder (Right $ Just rsp)  
-        (Nothing, _) -> responder $ Left $ responseError "Can't hover: Document is not a file"
-        (_, Nothing) -> responder $ Left $ responseError "Can't hover: No database connection"
+            [] -> liftLSP $ responder (Right Nothing)  
+            (isDefined :: Bool, name :: String, startLine :: Integer, startColumn :: Integer, endLine :: Integer, endColumn :: Integer):_ -> 
+              let ms = HoverContents $ markedUpContent "hstools" (T.pack $ name ++ if isDefined then " defined here" else "")
+                  range = LSP.Range (Position (fromIntegral startLine - 1) (fromIntegral startColumn - 1)) 
+                                    (Position (fromIntegral endLine - 1) (fromIntegral endColumn - 1))
+                  rsp = Hover ms (Just range)
+              in liftLSP $ responder (Right $ Just rsp)  
 
 
   , notificationHandler SCancelRequest $ const $ return ()
 
-  , notificationHandler (SCustomMethod "CleanDB") $ \message -> do
+  , notificationHandler (SCustomMethod "CleanDB") $ \message -> runInContext "CleanDB" $ do
       let NotificationMessage _ _ args = message
-      cfg <- LSP.getConfig
-      case (args, connection cfg) of
-        (A.Array (toList -> [ A.Null ]), Just conn) -> do
-          liftIO $ execute_ conn "drop table modules, names"
-          sendMessage "DB cleaned"
-        (A.Array (toList -> [ A.String s ]), Just conn)
-          -> sendMessage $ "Cleaning DB for path: " <> s
-        (_, Nothing) -> sendError "CleanDB needs DB connection"
-        (_, _) -> sendError $ T.pack $ "Unrecognized CleanDB argument: " ++ show args
+      withConnection $ \conn ->
+        case args of
+          A.Array (toList -> [ A.Null ]) -> do
+            liftIO $ execute_ conn "drop table modules, names"
+            sendMessage "DB cleaned"
+          A.Array (toList -> [ A.String s ])
+            -> sendMessage $ "Cleaning DB for path: " <> s
+          _ -> sendError $ T.pack $ "Unrecognized CleanDB argument: " ++ show args
   ]
+
+data LspContext = LspContext { ctOperation :: String }
+
+type LspMonad c = ReaderT LspContext (LspM c)
+
+liftLSP :: LspM c a -> LspMonad c a
+liftLSP = lift
+
+runInContext :: String -> LspMonad c a -> LspM c a
+runInContext operation action = runReaderT action (LspContext { ctOperation = operation })
+
+withConnection :: (Connection -> LspMonad Config ()) -> LspMonad Config ()
+withConnection act = do
+  operation <- asks ctOperation
+  cfg <- lift LSP.getConfig
+  case cfConnection cfg of
+    Just conn -> act conn
+    Nothing -> sendError $ T.pack $ operation ++ " needs DB connection"
+
+ensureFileLocation :: Uri -> (Either ResponseError a -> LspM Config ()) -> (FilePath -> LspMonad Config ()) -> LspMonad Config ()
+ensureFileLocation location responder action = case uriToFilePath location of
+  Just fp -> action fp
+  Nothing -> do
+    operation <- asks ctOperation
+    liftLSP $ responder $ Left $ responseError $ T.pack $ "Can't " ++ operation ++ ": Document is not a file"
 
 responseError m = ResponseError InvalidRequest m Nothing
 
-sendMessage = sendNotification SWindowShowMessage . ShowMessageParams MtInfo
-sendError = sendNotification SWindowShowMessage . ShowMessageParams MtError
+sendMessage :: T.Text -> LspMonad Config ()
+sendMessage = liftLSP . sendNotification SWindowShowMessage . ShowMessageParams MtInfo
 
-tryToConnectToDB :: LspM Config ()
+sendError :: T.Text -> LspMonad Config ()
+sendError = liftLSP . sendNotification SWindowShowMessage . ShowMessageParams MtError
+
+tryToConnectToDB :: LspMonad Config ()
 tryToConnectToDB = do
-  config <- LSP.getConfig
-  connOrError <- liftIO $ try $ connectPostgreSQL (BS.pack (postgresqlConnectionString config))
+  config <- liftLSP LSP.getConfig
+  connOrError <- liftIO $ try $ connectPostgreSQL (BS.pack (cfPostgresqlConnectionString config))
   case connOrError of
     Right conn -> do 
       sendMessage "Connected to DB"
-      LSP.setConfig (config { connection = Just conn })
+      liftLSP $ LSP.setConfig (config { cfConnection = Just conn })
     Left (e :: SomeException) -> return () -- error is OK
 
 main :: IO Int
@@ -144,16 +161,16 @@ main = runServer $ ServerDefinition
   { onConfigurationChange = loadConfig
   , doInitialize = \env _req -> pure $ Right env
   , staticHandlers = handlers
-  , interpretHandler = \env -> Iso (runLspT env) liftIO
+  , interpretHandler = \env -> Iso (runLspT env) (liftIO)
   , options = defaultOptions { executeCommandCommands = Just ["cleanDB"] }
-  , defaultConfig = Config "" Nothing
+  , defaultConfig = Config "" Nothing Nothing
   }
 
 
 loadConfig :: Config -> A.Value -> Either T.Text Config
 loadConfig config (A.Object (A.lookup "hstools" -> (Just (A.Object assoc))))
   = Right $ config 
-  { postgresqlConnectionString = fromMaybe (postgresqlConnectionString config) $
+  { cfPostgresqlConnectionString = fromMaybe (cfPostgresqlConnectionString config) $
       T.unpack <$> (fromString =<< psqlstr)
   }
   where
@@ -163,8 +180,9 @@ loadConfig config (A.Object (A.lookup "hstools" -> (Just (A.Object assoc))))
 loadConfig _ v = Left $ T.pack $ "Cannot parse options: " ++ show v
  
 data Config = Config
-  { postgresqlConnectionString :: String
-  , connection :: Maybe Connection
+  { cfPostgresqlConnectionString :: String
+  , cfConnection :: Maybe Connection
+  , cfOperation :: Maybe String
   } deriving (Show)
 
 instance Show Connection where
